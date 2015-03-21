1c1665932262a81c75a4300ee139e158b8989dff
Drew Fitzpatrick <drwftz@gmail.com>
2015-03-20 23:56:04 -0400

    Initial Commit

---
+++ BattleCards/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  BattleCards
+//
+
+//  Copyright (c) 2015 Drew Fitzpatrick. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
